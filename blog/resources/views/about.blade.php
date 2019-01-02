dd1f4cb839623029b33826b7f48616e8c1bd0b8a
ashraf1410499 <abuashraf501@gmail.com>
2019-01-02 23:14:30 +0600

    static page for testing

--- blog/resources/views/about.blade.php
+++ blog/resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Welcome Everyone
+                   this is about page
                 </div>
 
                 <div class="links">
--- blog/resources/views/contact.blade.php
+++ blog/resources/views/contact.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Welcome Everyone
+                   Email me at abuashraf501@gmail.com
                 </div>
 
                 <div class="links">
